8e4d4475c1c2ac5508cfda0aac40f69dba788b59
ryusuke.miyaji <bluce826@gmail.com>
2020-04-14 11:02:21 +0900

    initial commit

---
+++ web/resources/js/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// ページコンポーネントをインポートする
+import PhotoList from "./pages/PhotoList.vue";
+import Login from "./pages/Login.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: PhotoList
+  },
+  {
+    path: "/login",
+    component: Login
+  }
+];
+
+// VueRouterインスタンスを作成する
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
+
+export default router;
